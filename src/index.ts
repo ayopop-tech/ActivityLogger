3bcfad8ae0640b182bde1e707bf8481b7a19c11f
Priyanka <priyankakekane@ayopop.com>
2020-09-09 14:02:47 +0530

    Postobject changes for sending data to server

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 interface AppLogger {
-  emitEventLogs(loglevel: string, data: any) : any;
+  emitEventLogs(loglevel: string, data: any): any;
 }
 
 export default class ApplicationLogger implements AppLogger {
@@ -12,35 +12,33 @@ export default class ApplicationLogger implements AppLogger {
   ENV!: string;
   PARTNER_ID!: number;
 
- public init(initValues: any, partnerUserId : number){
-   this.APP = initValues.APPLICATION;
-   this.PARTNER_NAME = initValues.PARTNER;
-   this.ENV = initValues.ENV;
-   this.CONSOLE = initValues.CONSOLE;
-   this.URL = initValues.PUBLISH_URL;
-   this.PARTNER_ID = partnerUserId;
- }
+  public init(initValues: any, partnerUserId: number) {
+    this.APP = initValues.APPLICATION;
+    this.PARTNER_NAME = initValues.PARTNER;
+    this.ENV = initValues.ENV;
+    this.CONSOLE = initValues.CONSOLE;
+    this.URL = initValues.PUBLISH_URL;
+    this.PARTNER_ID = partnerUserId;
+  }
 
- async emitEventLogs(loglevel: string, data: any){
-     return this.publishDatatoServer(loglevel,data);
- }
- 
- private publishDatatoServer(loglevel: string, data: any) {
-  const header = {
-      "Content-Type": "application/json"
+  async emitEventLogs(loglevel: string, data: any) {
+    return this.publishDatatoServer(loglevel, data);
+  }
+
+  private publishDatatoServer(loglevel: string, data: any) {
+    const header = {
+      'Content-Type': 'application/json',
     };
-    
-  axios.post(this.URL, data)
-    .then((response: any) => {
-      // console.log(response);
-    })
-    .catch((error: any) => {
-      // console.log(error);
-    })
 
-
-
- }
+    axios
+      .post(this.URL, data)
+      .then((response: any) => {
+        // console.log(response);
+      })
+      .catch((error: any) => {
+        // console.log(error);
+      });
+  }
 }
 
 // let  loggerObject = {
@@ -50,7 +48,6 @@ export default class ApplicationLogger implements AppLogger {
 //  "CONSOLE" : "false",
 //  "PUBLISH_URL" : "https://dev0pg.openapi.ayopop.id/v1/partnermanager/api/product/updateproductdetails",
 // }
-
 
 // let initLogPri = new ApplicationLogger();
 // initLogPri.init(loggerObject,10);
